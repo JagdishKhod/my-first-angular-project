ee8953872b220112bb91de674e5fd53b07731a79
jagdishkhod <jagdishkhod892@gmail.com>
2023-06-15 18:52:50 +0530

    Generate NavBar Component by using comand

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './components/AboutComponent/about.component';
 import { FooterComponent } from './components/FooterComponent/footer.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
     FooterComponent,
+    NavBarComponent,
    
   
   ],
